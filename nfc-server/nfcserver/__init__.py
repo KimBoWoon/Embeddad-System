dc574c74398a53fd196468ad82128c9bbc1f6c88
KimBoWoon <haioer@naver.com>
2016-12-07 13:06:58 +0900

    modify blueprint

--- nfc-server/nfcserver/__init__.py
+++ nfc-server/nfcserver/__init__.py
@@ -5,8 +5,12 @@ def create_app():
     app = Flask(__name__)
 
     from nfcserver.db import DBManager
-    db_url = 'mysql+pymysql://root:rlaqhdns@localhost/nfc?charset=utf8'
+    db_url = 'mysql+pymysql://root:dkfrhflwma@localhost/nfc?charset=utf8'
     DBManager.init(db_url, True)
     DBManager.init_db()
+
+    from nfcserver.controller import index
+    from nfcserver.blueprint import nfc
+    app.register_blueprint(nfc)
 
     return app
--- nfc-server/nfcserver/blueprint.py
+++ nfc-server/nfcserver/blueprint.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-rainbow = Blueprint('nfc', __name__, template_folder='./templates', static_folder='./static')
+nfc = Blueprint('nfc', __name__, template_folder='./templates', static_folder='./static')
